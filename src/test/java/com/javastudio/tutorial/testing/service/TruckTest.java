dcd583c6e9bdcae2bf40f56d24df9afe7253e0b9
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2020-03-29 10:27:07 +0430

    Initial commit

---
+++ src/test/java/com/javastudio/tutorial/testing/service/TruckTest.java
@@ -0,0 +1,36 @@
+package com.javastudio.tutorial.testing.service;
+
+import com.javastudio.tutorial.testing.api.Engine;
+import com.javastudio.tutorial.testing.stub.MercedesBenzEngine;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+class TruckTest {
+
+    private Logger logger = Logger.getLogger(TruckTest.class.getName());
+
+    @Test
+    void truckWithStubEngine() {
+        logger.info("Testing truck starting and stopping");
+        Truck truck = new Truck(new MercedesBenzEngine());
+        truck.start();
+        assertTrue(truck.isStart());
+        truck.stop();
+        assertFalse(truck.isStart());
+    }
+
+    @Test
+    void truckStartingWithMockEngine(){
+        logger.info("Testing truck starting");
+        Engine mockEngine = mock(Engine.class);
+        when(mockEngine.isStart()).thenReturn(true);
+        Truck truck = new Truck(mockEngine);
+        assertTrue(truck.isStart());
+    }
+}
