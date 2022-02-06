e5993ce972fd5477d55af2c157ebd002bd939770
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-02-06 12:58:51 +0100

    Rename test class best practice

--- test-class-best-practice/src/test/java/com/javastudio/tutorial/ApplicationTest.java
+++ test-class-best-practice/src/test/java/com/javastudio/tutorial/ApplicationTest.java
@@ -1,28 +1,34 @@
 package com.javastudio.tutorial;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.Assert;
+import org.junit.jupiter.api.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ApplicationTest {
 
-    private Logger logger= LoggerFactory.getLogger(ApplicationTest.class);
+    private Logger logger = LoggerFactory.getLogger(ApplicationTest.class);
 
     public ApplicationTest() {
         logger.info("ApplicationTest instantiated!");
     }
 
     @Test
+    @DisplayName("This is the first test")
     void test1() {
 
     }
 
     @Test
+    @DisplayName("This is the second test")
     void test2() {
 
+    }
+
+    @Test
+    @Disabled("This test will not execute!")
+    void disabledTest() {
+        Assert.fail();
     }
 }
