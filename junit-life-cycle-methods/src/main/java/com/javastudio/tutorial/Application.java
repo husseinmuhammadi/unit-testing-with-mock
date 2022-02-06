3aa18878a7e8531de9b7fd0e7ea751a25bbb1ac6
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-02-06 12:59:36 +0100

    JUnit Test

---
+++ junit-life-cycle-methods/src/main/java/com/javastudio/tutorial/Application.java
@@ -0,0 +1,12 @@
+package com.javastudio.tutorial;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+    private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        logger.info("Application started!");
+    }
+}
